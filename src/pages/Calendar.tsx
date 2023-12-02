dc2b269cf1676bdfc2b1812da8724570f5cf91d8
songmok <songmokseed@gmail.com>
2023-12-02 21:28:42 +0900

    카테고리 생성 및 조회 적용

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,15 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Calendar = () => {
-  // 해당 부분 컴포넌트 때어내기
   const navigate = useNavigate();
+  const [cateName, setCateName] = useState("");
+  const [message, setMessage] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  const cateGet = () => {
+    axios
+      .get("http://localhost:5000/api/cate/categet")
+      .then((response) => {
+        const cateData = response.data.categories;
+        console.log("ca", cateData);
+        setCategories(cateData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  const cateCt = () => {
+    const data = {
+      cateName: cateName,
+    };
+    axios
+      .post("http://localhost:5000/api/cate/categories", data)
+      .then((response) => {
+        setMessage(response.data.message);
+        console.log("res", response);
+        cateGet();
+      })
+      .catch((error) => {
+        console.error(error);
+        setMessage("카테고리 생성 실패");
+      });
+  };
+
+  useEffect(() => {
+    cateGet();
+  }, []);
+
   return (
     <>
       <article className="container m-auto">
+        <div>
+          <input
+            type="text"
+            placeholder="카테고리 이름"
+            value={cateName}
+            onChange={(e) => setCateName(e.target.value)}
+          />
+          <button onClick={cateCt}>카테고리 생성</button>
+          <p>{message}</p>
+        </div>
+        <div>
+          <h2>카테고리 목록</h2>
+          {categories.map((v: any, i) => (
+            <>
+              <div className="ca" key={i}>
+                {v.cateName}
+              </div>
+            </>
+          ))}
+        </div>
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin]}
           headerToolbar={{
